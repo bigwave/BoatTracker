7bba698e74dba3dc7c4ae2d949fdc469f6c8f589
Ian Fraser <ian@glenborrodale.co.uk>
2023-10-13 12:57:03 +0100

    Change TTN appEUI

--- include/version.h
+++ include/version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.38"
+        #define VERSION "0.1.39"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-10-13 12:45:35.908152"
+        #define BUILD_TIMESTAMP "2023-10-13 12:54:39.967656"
     #endif
     
